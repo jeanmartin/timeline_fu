92d8b8ec05c357260be5e3afc3f61688e58ba9a9
Ryan Michael <ryan@bcarc.com>
2010-02-19 21:28:25 -0600

    couple refactors to make the code more legible

--- lib/timeline_fu/has_timeline.rb
+++ lib/timeline_fu/has_timeline.rb
@@ -6,39 +6,42 @@ module TimelineFu
     end
 
     module InstanceMethods
-      attr_accessor :timeline_association_scopes, :timeline_association_orders
-      
       private
       
       def handle_dependent_timelines
-        self.class.timeline_association_dependent.each_pair do |name,dependent|
+        self.class.tl_dependent.each_pair do |name,dependent|
           case dependent
-            when nil then return
-            when :destroy then 
-              self.send(name).each{|event| event.destroy()}
-            when :delete_all then
-              self.send(name).each{|event| event.delete()}
-            else raise ArgumentError, "Not supported value for :dependent (#{dependent})"
+            when nil          then return
+            when :destroy     then self.send(name).each{|event| event.destroy()}
+            when :delete_all  then self.send(name).each{|event| event.delete()}
+            else              raise ArgumentError, "Not supported value for :dependent (#{dependent})"
           end
         end
       end
       
       def method_missing(method_id)    
-        unless self.class.timeline_association_scopes.nil?  
-          if self.class.timeline_association_scopes.key?( method_id )
-            dynamic_scope = self.class.timeline_association_scopes[method_id].map{|as| self.class.generate_scope_name(method_id, as)}.join("_or_")
-            scoped_tl = TimelineEvent.send( dynamic_scope, self.id )
-            
-            if self.class.timeline_association_orders.key?( method_id ) && scoped_tl.condition?(self.class.timeline_association_orders[method_id] )
-              return scoped_tl.send( self.class.timeline_association_orders[method_id] )
-            else
-              return scoped_tl
-            end
+        unless self.class.tl_scopes.nil?  
+          if self.class.tl_scopes.key?( method_id )
+            get_scoped_events(method_id)
           else
             super
           end
         end
       end  
+      
+      def get_scoped_events(association)
+        dynamic_scopes = self.class.tl_scopes[association].map do 
+          |as| self.class.generate_scope_name(association, as)
+        end
+        
+        scoped_tl = TimelineEvent.send( dynamic_scopes.join("_or_"), self.id )
+        
+        if self.class.tl_orders.key?( association ) && scoped_tl.condition?(self.class.tl_orders[association] )
+          return scoped_tl.send( self.class.tl_orders[association] )
+        else
+          return scoped_tl
+        end
+      end      
     end
         
     module ClassMethods    
@@ -48,47 +51,13 @@ module TimelineFu
           :order,
           :dependent
         )  
-        @timeline_association_scopes ||= {}
-        @timeline_association_orders ||= {}
-        @timeline_association_dependent ||={}
-        class << self; attr_reader :timeline_association_scopes; end    
-        class << self; attr_reader :timeline_association_orders; end  
-        class << self; attr_reader :timeline_association_dependent; end  
         
-        @timeline_association_scopes[name] = []
-        @timeline_association_orders[name] = opts[:order] if opts.key?( :order )
+        initialize_variables if @tl_scopes.nil?
+        @tl_scopes[name]      = []
+        @tl_orders[name]      = opts[:order] if opts.key?( :order )
+        @tl_dependent[name]   = opts[:dependent]
 
-        @timeline_association_dependent[name] = opts[:dependent]
-        before_destroy :handle_dependent_timelines
-        
-        case 
-          when opts[:as].kind_of?(Hash) then
-            [:actor, :subject, :secondary_subject].each do |as|
-              if (opts[:as].key? as)
-                generate_as_scope(name, as, opts[:as][as]) 
-                @timeline_association_scopes[name] << as
-              end
-            end
-            
-          when opts[:as].kind_of?(Array) then
-            [:actor, :subject, :secondary_subject].each do |as|
-              if (opts[:as].include? as)
-                generate_as_scope(name, as)
-                @timeline_association_scopes[name] << as
-              end
-            end       
-               
-          when opts[:as] == :all then
-            [:actor, :subject, :secondary_subject].each do |as|
-              generate_as_scope(name, as)
-              @timeline_association_scopes[name] << as
-            end
-            
-          when [:actor, :subject, :secondary_subject].include?(opts[:as]) then
-            generate_as_scope(name, opts[:as])
-            @timeline_association_scopes[name] << opts[:as]
-          else raise ArgumentError, "Argument :as is mandatory and must be one of [:actor | :subject | :secondary_subject]"
-        end
+        generate_scopes(name, opts[:as])
       end
 
       def generate_scope_name(association_name, as)
@@ -96,6 +65,45 @@ module TimelineFu
       end
             
       private
+      
+      def initialize_variables
+        before_destroy :handle_dependent_timelines
+        class << self; attr_reader :tl_scopes, :tl_orders, :tl_dependent; end
+        @tl_scopes          ||= {}
+        @tl_orders          ||= {}
+        @tl_dependent       ||= {}
+      end      
+      
+      def generate_scopes(name, as)
+        case 
+          when as.kind_of?(Hash) then
+            [:actor, :subject, :secondary_subject].each do |a|
+              if (as.key? a)
+                generate_as_scope(name, a, as[a]) 
+                @tl_scopes[name] << a
+              end
+            end
+            
+          when as.kind_of?(Array) then
+            [:actor, :subject, :secondary_subject].each do |a|
+              if (as.include? a)
+                generate_as_scope(name, a)
+                @tl_scopes[name] << a
+              end
+            end       
+               
+          when as == :all then
+            [:actor, :subject, :secondary_subject].each do |a|
+              generate_as_scope(name, a)
+              @tl_scopes[name] << a
+            end
+            
+          when [:actor, :subject, :secondary_subject].include?(as) then
+            generate_as_scope(name, as)
+            @tl_scopes[name] << as
+          else raise ArgumentError, "Argument :as is mandatory and must be one of [:actor | :subject | :secondary_subject]"
+        end      
+      end
       
       def generate_as_scope(association_name, as, conditions = nil)
         class_name = self.base_class.name
